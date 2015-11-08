9074e3a5729f2ef6fea9a62e58479f57f4ac5efa
netoeuler <eulerneto.ngt@gmail.com>
2015-11-08 14:33:28 -0300

    q13

--- picgbib/picgbib.py
+++ picgbib/picgbib.py
@@ -2,11 +2,12 @@ import numpy
 import matplotlib as mp
 import matplotlib.pyplot as plt
 import Image
+import math
 
 debug=False
 
 def imread(nomeArquivo):
-	return numpy.asarray(Image.open("samples/"+nomeArquivo))
+	return numpy.asarray(Image.open(nomeArquivo))
 
 def imshow(image):
 	if (image.shape[0] < 50):
@@ -42,7 +43,7 @@ def rgb2gray(image):
 	return numpy.dot(image[...,:3], [0.299, 0.587, 0.144]).astype(numpy.uint8)
 
 def imreadgray(nomeArquivo):
-	image = numpy.asarray(Image.open("samples/"+nomeArquivo))
+	image = numpy.asarray(Image.open(nomeArquivo))
 	if isgrayscale(image):
 		return image
 	else:
@@ -65,13 +66,51 @@ def hist(image, bin):
 		b=image[...,2].flatten()
 		return plt.hist([r,g,b], bin, histtype='bar', color=['r','g','b'])
 		
-def showhist (hist):
+def showhist(hist):
 	#plt.legend()
 	return plt.show()
 
+#Falta a letra 'a'
+def contrast(image,r,m):
+	n = len(image)
+
+	g = [0.0] * n
+
+	for i in range(n):
+		g[i] = r*image[i]+m
+
+	return numpy.asarray(g)
+
+#under construction
+def dft(image):
+	n = len(image)
+
+	inreal = image;
+	inimag = [complex(0)] * n;
+
+	outreal = [0.0] * n
+	outimag = [0.0] * n
+
+	for k in range(n):
+		sumreal = 0.0
+		sumimag = 0.0
+		for t in range(n):
+			angle = 2*math.pi*t*k/n
+			sumreal +=  inreal[t] * math.cos(angle) + inimag[t] * math.sin(angle)
+			sumimag += -inreal[t] * math.sin(angle) + inimag[t] * math.cos(angle)
+        outreal[k] = sumreal
+        outimag[k] = sumimag
+
+	output = [0.0] * n
+
+	for k in range(n):
+		output[k] = outreal[k] + outimag[k]
+
+	return numpy.asarray(output)
+
 #>>> LINHA DE COMANDO
-img = imread("lena_std.tif")
-img2 = thresh(img,150)
-imshow(img2)
+img = imread("exercicio_fft/lena1.jpg")
+#imshow(contrast(img,5,2))
+imshow(dft(img))
 #img2 = hist(img,256)
 #showhist(img2)
